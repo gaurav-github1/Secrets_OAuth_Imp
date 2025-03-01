338ee813d3f1ecf19bf44c5d482a570156eb5ecf
Gaurav Verma <gv29403@gmail.com>
2025-03-01 19:36:13 +0530

    added dotenv

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ import passport from "passport";
 import { Strategy } from "passport-local";
 import GoogleStrategy from "passport-google-oauth2";
 import session from "express-session";
-import env from "dotenv";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const port = 3000;
 const saltRounds = 10;
-env.config();
 
 app.use(
   session({
